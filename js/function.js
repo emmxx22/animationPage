cdd759152a0b645a9d10a5ed5c8f762f85c1a2ef
emmxx22 <59826844+emmxx22@users.noreply.github.com>
2020-04-04 11:25:39 +0100

    added little features

---
+++ js/function.js
@@ -0,0 +1,5 @@
+new TypeIt(".description", {
+    strings: ["This is a one-page website with a start screen and display animations", "There are so many fun things you can do with css"],
+    speed: 200,
+    waitUntilVisible: true
+  }).go();
